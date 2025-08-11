b30b15be754d5e0a7785d99fed020cffbcd6c7a2
오정환 <hwaning1@douzone.com>
2025-08-11 23:11:48 +0900

    :recycle: Controller 통합 및 정리

--- src/main/java/com/lagavulin/yoghee/config/SecurityConfig.java
+++ src/main/java/com/lagavulin/yoghee/config/SecurityConfig.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,17 +15,18 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthFilter jwtAuthFilter = new JwtAuthFilter();
+    private final JwtAuthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/swagger-ui/**", "/api/auth/**","/login/**").permitAll()
-                .anyRequest().authenticated())
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            .httpBasic(Customizer.withDefaults())
-            .formLogin(form -> form.loginPage("/ddd")
-                .permitAll()
-            ).build();
+                   .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                   .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui.html","/swagger-ui/**","/swagger-resources/**", "/v3/api-docs/**", // Swagger
+                            "/auth/**", // 인증
+                            "/login/page", "/images/*.png" // TODO DELETE 예정
+                        ).permitAll()
+                        .anyRequest().authenticated())
+                   .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                   .build();
     }
 }
